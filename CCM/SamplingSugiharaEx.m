70afe11b2bd7215ac07966f066010b3bd8b83633
Jim <jim@AstroOrbiter.(none)>
2013-12-18 12:04:30 -0500

    Added plot commands

--- CCM/SamplingSugiharaEx.m
+++ CCM/SamplingSugiharaEx.m
@@ -1,4 +1,4 @@
-number_of_samples = 1000;
+number_of_samples = 3000;
 %seed_value_rx = 12;
 %seed_value_ry = 34;
 %seed_value_Xo = 56;
@@ -7,11 +7,11 @@ mean_rx = 3.8;%3.8;
 mean_ry = 3.5;%3.5;
 mean_Xo = 0.4;%0.4;
 mean_Yo = 0.2;%0.2;
-var_rx = 0.1;
-var_ry = 0.1;
-var_Xo = 0.3;
-var_Yo = 0.3;
-library_length = [1:50:1000];
+var_rx = 0.05;
+var_ry = 0.05;
+var_Xo = 0.1;
+var_Yo = 0.1;
+library_length = [1000];
 
 Bxy = 0.02;%0.02;
 Byx = 0.1;%0.1;
@@ -52,16 +52,27 @@ for llstep = 1:1:length(library_length),
 end;
 
 %plotdataCCM
-%{
+
+tolerance = 1E-12;
+plotdata_filtered = plotdataCCM(1,abs(plotdataCCM(1,:)) >= tolerance);
 figure(1);
 subplot(2,1,1);
-plot(1:1:number_of_samples,plotdataCCM,'.',1:1:number_of_samples,mean(plotdataCCM),'-',1:1:number_of_samples,mean(plotdataCCM)+std(plotdataCCM),'.-',1:1:number_of_samples,mean(plotdataCCM)-std(plotdataCCM),'.-');
+plot(1:1:number_of_samples,plotdata_filtered,'.',...
+     1:1:number_of_samples,mean(plotdata_filtered),'-',...
+     1:1:number_of_samples,mean(plotdata_filtered)+std(plotdata_filtered),'.-',...
+     1:1:number_of_samples,mean(plotdata_filtered)-std(plotdata_filtered),'.-');
+title('Samples shown with Mean and Mean+Std Bounds');
+xlabel('sample number');
+ylabel('\Delta');
 subplot(2,1,2);
-hist(plotdataCCM,100)
-%}
+hist(plotdata_filtered,100)
+title('Histogram of 3000 samples with L = 1000');
+xlabel('\Delta bins');
+ylabel('counts');
+
 
 %imagesc(plotdataCCM);
-
+%{
 tolerance = 1E-2;
 histimageplot = zeros(19,100);
 temp = plotdataCCM(20,abs(plotdataCCM(20,:)) >= tolerance);
@@ -70,3 +81,4 @@ for stepstep = 1:18,
     temp = plotdataCCM(stepstep+1,abs(plotdataCCM(stepstep+1,:)) >= tolerance);
     [histimageplot(stepstep,:),centers] = hist(temp,centers);
 end;
+%}
