3bee05976e051f85fb33e84ae8ef0884fc0b3244
Jim <jmccrac1@masonlive.gmu.edu>
2014-03-26 20:38:57 -0400

    Adding threading to CCM code (CURRENTLY DOES NOT COMPILE)

---
+++ CCM/Ccode/ccmth.cpp
@@ -0,0 +1,264 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include "ccm.h"
+#include <thread>
+#include <future>
+
+using namespace std;
+
+// Simple functions to help stay organized
+void CmdLineHelp();
+bool ReadCmdLineArgs(int,char**);
+double* CCMcorr_async(double*,double*);
+
+// Globals read from command line
+int iEmbeddingDimension = -1,
+    iLagTimeStep = -1,
+    iLibraryLength = -1,
+    iNumOfTimeSeries = -1,
+    iNumThreads = -1;
+
+char *cOutputname,
+     *cFilename; //this data file is assumed to be in the proper format (see "usage")
+
+bool bVerboseFlag = false;
+
+// Main
+int main(int argc, char **argv){
+
+    bool bCmdLineOK = ReadCmdLineArgs(argc,argv);
+    if( !bCmdLineOK ){
+        return(-1);
+    }
+
+    char cBuffer[1024]; //read in buffer
+    char *cColValue, //used for read in
+         *cRowValues; //used for read in
+    int iter = 0;
+    double dX[iLibraryLength],
+           dY[iLibraryLength];
+
+    //Declare things needed for threading
+    double dCCMcorrs[iNumThreads][2];
+    future<double> WorkerThreads[iNumThreads];
+
+    if( bVerboseFlag ){
+            printf("Input data file: %s\nOutput file: %s\nNumber of time series in input file: %i\nLibrary length: %i\nEmbedding dimension: %i\nLag time step: %i\n",cFilename,cOutputname,iNumOfTimeSeries,iLibraryLength,iEmbeddingDimension,iLagTimeStep);
+    }
+
+    //open data input faile
+    FILE *ifstream = fopen(cFilename,"r");
+    if(ifstream == NULL){
+        fprintf(stderr, "Error: Cannot open %s\n",cFilename);
+            return(-1);
+    }
+
+    //open output file
+    FILE *ofstream = fopen(cOutputname,"w");
+    if(ofstream == NULL){
+        fprintf(stderr, "Error: Cannot open %s\n",cFilename);
+        return(-1);
+    }
+
+    if( bVerboseFlag ){ printf("Processing %s...\n",cFilename); }
+    int iCurrentThreadsUsed = 0;
+    for(int iTSiter = 0;iTSiter < iNumOfTimeSeries;iTSiter++ ){
+
+	if( bVerboseFlag ){ printf("TS %i:",iTSiter); }
+	if( bVerboseFlag ){ printf("Reading TS..."); }
+
+        //read in dX and dY
+        for( iter=0;iter < iLibraryLength;iter++ ){
+		cRowValues = fgets(cBuffer,sizeof(cBuffer),ifstream);
+		cColValue = strtok(cRowValues,",");
+		if( cColValue != NULL ){
+		   dX[iter] = atof(cColValue);
+		}else{
+		   fprintf(stderr, "Error: %s is not formatted correctly\n",cFilename);
+		}
+
+		cColValue = strtok(NULL,";");
+		if( cColValue != NULL ){
+		   dY[iter] = atof(cColValue);
+		}else{
+		   fprintf(stderr, "Error: %s is not formatted correctly\n",cFilename);
+		}
+        }
+
+        if( bVerboseFlag ){ printf("Writing CCMs..."); }
+
+        if( iNumThreads == 0 ){
+
+            fprintf(ofstream,"%.20f,",CCMcorr(dX,iLibraryLength,dY,iLibraryLength,iEmbeddingDimension,iLagTimeStep));
+            fprintf(ofstream,"%.20f\n",CCMcorr(dY,iLibraryLength,dX,iLibraryLength,iEmbeddingDimension,iLagTimeStep));
+
+        }else{
+            if( iCurrentThreadsUsed < iNumThreads ){
+
+                WorkerThreads[iCurrentThreadsUsed] = async(launch::async,CCMcorr_async,dX,dY);
+                iCurrentThreadsUsed++;
+
+            }else{
+
+                for(int iThreadIter = 0;iThreadIter < iNumThreads;iNumThreads++ ){
+                    dCCMcorrs[iThreadIter] = WorkerThreads[iThreadIter].get();
+                }
+
+                iCurrentThreadsUsed = 0;
+                WorkerThreads[iCurrentThreadsUsed] = async(launch::async,CCMcorr_async,dX,dY);
+
+                for(int iThreadIter = 0;iThreadIter < iNumThreads;iNumThreads++ ){
+                    fprintf(ofstream,"%.20f,",dCCMcorrs[iThreadIter][0]);
+                    fprintf(ofstream,"%.20f\n",dCCMcorrs[iThreadIter][1]);
+                    dCCMcorrs[iThreadIter][0] = nan;
+                    dCCMcorrs[iThreadIter][1] = nan;
+                }
+            }
+        }
+
+        if( bVerboseFlag ){ printf(" done.\n"); }
+
+    }
+    fclose(ifstream);
+    fclose(ofstream);
+
+    if( bVerboseFlag ){ printf(" Finished.\n"); }
+
+    return(0);
+}
+
+void CmdLineHelp(char* cName){
+
+    printf("usage: %s -E [integer1] -t [integer2] -L [integer3] -f [filename1] -n [integer4] -p [integer5] -o [filename2] -v\n",cName);
+    printf("  [integer1]  = embedding dimension\n");
+    printf("  [integer2]  = lag time step\n");
+    printf("  [integer3]  = library length of the time series (assumed to be equal for X and Y)\n");
+    printf("  [filename1] = filename of text file containing time series data for X and Y with\n");
+    printf("                columns seperated by commas and rows seperated by semicolons (it is\n");
+    printf("                assumed that there are only X and Y in the file, i.e. only two columns)\n");
+    printf("  [integer4]  = number of time series in the file\n");
+    printf("  [integer5]  = number of threads to use (default is zero)\n");
+    printf("  [filename2] = filename of text file containing ouput CCM correlations\n");
+    printf("  -v flag provides various bits of information sent to STDOUT\n");
+    printf("input data file format:  X0,Y0;\n");
+    printf("                                 X1,Y1;\n");
+    printf("                                 X2,Y2;\n");
+    printf("                                 X3,Y3;\n");
+    printf("                                 X4,Y4;\n");
+    printf("                          ...\n");
+    printf("output data file format: CCM(X,Y),CCM(Y,X) [time series 1]\n");
+    printf("                         CCM(X,Y),CCM(Y,X) [time series 2]\n");
+    printf("                                ...\n");
+
+}
+
+bool ReadCmdLineArgs(int argc, char **argv){
+
+    bool bInputSet = false,
+         bOutputSet = false;
+
+    for(int iter=1;iter < argc;iter++ ){
+
+        if( (strcmp("-h",argv[iter]) == 0) || (strcmp("-?",argv[iter]) == 0) ){
+            CmdLineHelp(argv[0]);
+            return( false );
+
+        }else if( strcmp("-E",argv[iter]) == 0 ){
+
+            iEmbeddingDimension = atoi(argv[iter+1]);
+
+        }else if( strcmp("-t",argv[iter]) == 0 ){
+
+            iLagTimeStep = atoi(argv[iter+1]);
+
+        }else if( strcmp("-L",argv[iter]) == 0 ){
+
+            iLibraryLength = atoi(argv[iter+1]);
+
+        }else if( strcmp("-f",argv[iter]) == 0 ){
+
+            cFilename = argv[iter+1];
+            bInputSet = true;
+
+        }else if( strcmp("-n",argv[iter]) == 0 ){
+
+            iNumOfTimeSeries = atoi(argv[iter+1]);
+
+        }else if( strcmp("-p",argv[iter]) == 0 ){
+
+            iNumThreads = atoi(argv[iter+1]);
+
+        }else if( strcmp("-o",argv[iter]) == 0 ){
+
+            cOutputname = argv[iter+1];
+            bOutputSet = true;
+
+        }else if( strcmp("-v",argv[iter]) == 0 ){
+
+            bVerboseFlag = true;
+
+        }else{
+
+            CmdLineHelp(argv[0]);
+            return( false );
+
+        }
+    }
+
+    //Make sure the required things were defined
+    if( iEmbeddingDimension == -1 ){
+        fprintf(stderr, "Error: embedding dimension is not defined\n\n");
+        CmdLineHelp(argv[0]);
+        return( false );
+    }
+    if( iLagTimeStep == -1 ){
+        fprintf(stderr, "Error: lag time step is not defined\n\n");
+        CmdLineHelp(argv[0]);
+        return( false );
+    }
+    if( iLibraryLength == -1 ){
+        fprintf(stderr, "Error: library length is not defined\n\n");
+        CmdLineHelp(argv[0]);
+        return( false );
+    }
+    if( !bInputSet ){
+        fprintf(stderr, "Error: input data filename is not defined\n\n");
+        CmdLineHelp(argv[0]);
+        return( false );
+    }
+    if( iNumOfTimeSeries == -1 ){
+        fprintf(stderr, "Error: number of time series in input data file not defined\n\n");
+        CmdLineHelp(argv[0]);
+        return( false );
+    }
+    if( !bOutputSet ){
+        fprintf(stderr, "Error: output data filename is not defined\n\n");
+        CmdLineHelp(argv[0]);
+        return( false );
+    }
+
+    //Issue any warnings
+    if( iNumThreads == -1 ){
+        fprintf(stderr, "Warning: number of threads not defined; using default\n");
+        iNumThreads = 0;
+    }
+    if( bVerboseFlag && (iNumThreads != 0) ){
+        fprintf(stderr, "Warning: verbose logging is not available with multiple threads; turning off verbose output\n");
+        bVerboseFlag = false;
+    }
+
+    return( true );
+}
+
+double* CCMcorr_async(double dX[],double dY[]){
+
+    double dOutput[2];
+
+    dOutput[0] = CCMcorr(dX,iLibraryLength,dY,iLibraryLength,iEmbeddingDimension,iLagTimeStep);
+    dOutput[1] = CCMcorr(dY,iLibraryLength,dX,iLibraryLength,iEmbeddingDimension,iLagTimeStep);
+
+    return( dOutput );
+}
+
