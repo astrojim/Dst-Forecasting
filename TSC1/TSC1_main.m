4c66d758f8d79bacca216c293b3f5ed0ab73696b
Jim <jmccrac1@masonlive.gmu.edu>
2014-08-05 19:40:49 -0400

    Added directory for testing the new algorithm

---
+++ TSC1/TSC1_main.m
@@ -0,0 +1,39 @@
+t = [0:0.01:500];
+pulses = [100:100:500];
+%x = sin(t);
+%x = square(t);
+%x = sawtooth(t);
+%x = pulstran(t,pulses,'tripuls');
+x = pulstran(t,pulses,'gauspuls');
+
+xd = zeros(length(x)-1,1);
+y = zeros(length(x)+1,1);
+for iter = 1:1:(length(x)-1),
+	xd(iter,1) = x(iter+1)-x(iter);
+end;
+
+for iter = 2:1:length(x)+1,
+	y(iter) = x(iter-1) + 0.1*rand();
+    %y(iter) = x(iter-1) + 0.9*rand();
+    %y(iter) = rand();
+end;
+
+%plot(x)
+%plot(y)
+[tstruct,cnts] = hist_extra(xd,100);
+bar(cnts)
+%hist(xd,10)
+
+for iter = 1:1:length(tstruct),
+	
+    %for tstep = 1:1:tstruct(iter).tcount
+    %	printf('%i,',tstruct(iter).tsteps(tstep));
+    %end;
+    %printf('\n');
+    
+    if( tstruct(iter).tcount ~= 0 ),
+        fprintf('Bin %i\n',iter);
+        figure(iter);
+        hist(y(tstruct(iter).tsteps+2),100);
+    end;
+end;
---
+++ TSC1/hist_extra.m
@@ -0,0 +1,25 @@
+function [tsteps_struct,counts] = hist_extra(TSdata,nbins)
+
+Xmax = max(TSdata);
+X_stepsize = abs(Xmax-min(TSdata))/nbins;
+
+for iter = 1:1:nbins,
+	tsteps_struct(iter).binUpper = nan;
+    tsteps_struct(iter).binLower = nan;
+    tsteps_struct(iter).tcount = 0;
+    tsteps_struct(iter).tsteps = nan;
+end;
+counts = zeros(nbins);
+
+for Xstep = 1:1:length(TSdata),
+    for Bstep = 1:1:nbins,
+       if(TSdata(Xstep) >= (Xmax-(Bstep*X_stepsize))),
+        	tsteps_struct(nbins-(Bstep-1)).binUpper = (Xmax-((Bstep-1)*X_stepsize));
+            tsteps_struct(nbins-(Bstep-1)).binLower = (Xmax-(Bstep*X_stepsize));
+            tsteps_struct(nbins-(Bstep-1)).tcount = tsteps_struct(nbins-Bstep+1).tcount + 1;
+            tsteps_struct(nbins-(Bstep-1)).tsteps(tsteps_struct(nbins-Bstep+1).tcount) = Xstep;
+            counts(nbins-(Bstep-1)) = counts(nbins-(Bstep-1))+1;
+            break;
+        end;
+   end;
+end;
