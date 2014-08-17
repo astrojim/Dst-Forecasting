d63828fcfad304652c4451d9a01e58c332a1e26f
Jim <jmccrac1@masonlive.gmu.edu>
2014-08-17 19:50:03 -0400

    Updating the main script

--- main.m
+++ main.m
@@ -1,22 +1,25 @@
 addpath('./TSC1');
 
-fprintf('X and Y have no link\n');
+binnum = 64;
+
 t = [0:0.01:500];
 x = sin(t);
 
 xd = zeros(length(x)-1,1);
-y = zeros(length(x)+1,1);
 for iter = 1:1:(length(x)-1),
 	xd(iter,1) = x(iter+1)-x(iter);
 end;
 
+fprintf('dX : X and Y have no link\n');
+
+y = zeros(length(x)+1,1);
 for iter = 2:1:length(x)+1,
     y(iter) = rand();
 end;
 
-[tstruct,cnts] = hist_extra(xd,16);
+[tstruct,cnts] = hist_extra(xd,binnum);
 
-figure('Name','Y is random');
+figure('Name','dX : Y is random');
 for iter = 1:1:length(tstruct),
     if( tstruct(iter).tcount ~= 0 ),
         fprintf('  Bin %i [%.5f,%.5f]\n',iter,tstruct(iter).binLower,tstruct(iter).binUpper);
@@ -26,28 +29,67 @@ for iter = 1:1:length(tstruct),
     end;
 end;
 
-fprintf('X drives Y\n');
-t = [0:0.01:500];
-x = sin(t);
+fprintf('dX : X drives Y\n');
 
-xd = zeros(length(x)-1,1);
 y = zeros(length(x)+1,1);
-for iter = 1:1:(length(x)-1),
-	xd(iter,1) = x(iter+1)-x(iter);
-end;
-
 for iter = 2:1:length(x)+1,
     y(iter) = x(iter-1) + 0.1*rand();
 end;
 
-[tstruct,cnts] = hist_extra(xd,16);
+[tstruct,cnts] = hist_extra(xd,binnum);
 
-figure('Name','X drives Y');
+figure('Name','dX : X drives Y');
 for iter = 1:1:length(tstruct),
     if( tstruct(iter).tcount ~= 0 ),
         fprintf('  Bin %i [%.5f,%.5f]\n',iter,tstruct(iter).binLower,tstruct(iter).binUpper);
         subplot(sqrt(length(tstruct)),sqrt(length(tstruct)),iter);
         hist(y(tstruct(iter).tsteps+2),100);
+        xlim([-1 1]);
+    end;
+end;
+
+fprintf('dY : X and Y have no link\n');
+
+y = zeros(length(x)+1,1);
+for iter = 2:1:length(x)+1,
+    y(iter) = rand();
+end;
+yd = zeros(length(y)-1,1);
+for iter = 1:1:(length(y)-1),
+	yd(iter,1) = y(iter+1)-y(iter);
+end;
+
+[tstruct,cnts] = hist_extra(yd,binnum);
+
+figure('Name','dY : Y is random');
+for iter = 1:1:length(tstruct),
+    if( tstruct(iter).tcount ~= 0 ),
+        fprintf('  Bin %i [%.5f,%.5f]\n',iter,tstruct(iter).binLower,tstruct(iter).binUpper);
+        subplot(sqrt(length(tstruct)),sqrt(length(tstruct)),iter);
+        hist(x(tstruct(iter).tsteps),100);
+        xlim([-1 1]);
+    end;
+end;
+
+fprintf('dY : X drives Y\n');
+
+y = zeros(length(x)+1,1);
+for iter = 2:1:length(x)+1,
+    y(iter) = x(iter-1) + 0.1*rand();
+end;
+yd = zeros(length(y)-1,1);
+for iter = 1:1:(length(y)-1),
+	yd(iter,1) = y(iter+1)-y(iter);
+end;
+
+[tstruct,cnts] = hist_extra(yd,binnum);
+
+figure('Name','dY : X drives Y');
+for iter = 1:1:length(tstruct),
+    if( tstruct(iter).tcount ~= 0 ),
+        fprintf('  Bin %i [%.5f,%.5f]\n',iter,tstruct(iter).binLower,tstruct(iter).binUpper);
+        subplot(sqrt(length(tstruct)),sqrt(length(tstruct)),iter);
+        hist(x(tstruct(iter).tsteps),100);
         xlim([-1 1]);
     end;
 end;
