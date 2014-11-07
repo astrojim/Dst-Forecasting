caabd44017d33c67b1cdcde61b85db2ef36a3012
Jim <jmccrac1@masonlive.gmu.edu>
2014-11-06 19:13:32 -0500

    Adding script for higher order histograms

---
+++ TSC1/histjoint3_extraWBIN.m
@@ -0,0 +1,45 @@
+function [tsteps_struct,counts] = histjoint3_extraWBIN(TSdataX,TSdataY,TSdatadX,...
+    binUpperX,binLowerX,binUpperY,binLowerY,binUpperdX,binLowerdX)
+% It is assumed that X and Y are the same length
+
+nbins = length(binUpperX);
+
+for iter = 1:1:nbins,
+    tsteps_struct(iter).binUpperX = nan;
+    tsteps_struct(iter).binLowerX = nan;
+    tsteps_struct(iter).binUpperY = nan;
+    tsteps_struct(iter).binLowerY = nan;
+    tsteps_struct(iter).binUpperdX = nan;
+    tsteps_struct(iter).binLowerdX = nan;
+    tsteps_struct(iter).tsteps = nan;
+    tsteps_struct(iter).tcount = 0;
+end;
+counts = zeros(nbins,nbins,nbins);
+
+for BstepX = 1:1:length(binUpperX),
+    for BstepY = 1:1:length(binUpperY),
+        for BstepdX = 1:1:length(binUpperdX),
+
+            for Xstep = 1:1:length(TSdataX),
+
+                if((TSdataX(Xstep) >= binLowerX(BstepX)) && (TSdataX(Xstep) < binUpperX(BstepX))...
+                 && (TSdataY(Xstep) >= binLowerY(BstepY)) && (TSdataY(Xstep) <  binUpperY(BstepY))...
+                 && (TSdatadX(Xstep) >= binLowerdX(BstepdX)) && (TSdatadX(Xstep) < binUpperdX(BstepdX))),
+
+                    tsteps_struct(nbins-(BstepX-1)).binUpperX = binUpperX(BstepX);
+                    tsteps_struct(nbins-(BstepX-1)).binLowerX = binLowerX(BstepX);
+                    tsteps_struct(nbins-(BstepX-1)).binUpperY = binUpperY(BstepY);
+                    tsteps_struct(nbins-(BstepX-1)).binLowerY = binLowerY(BstepY);
+                    tsteps_struct(nbins-(BstepX-1)).binUpperdX = binUpperdX(BstepdX);
+                    tsteps_struct(nbins-(BstepX-1)).binLowerdX = binLowerdX(BstepdX);
+                    
+                    tsteps_struct(nbins-(BstepX-1)).tcount = tsteps_struct(nbins-BstepX+1).tcount + 1;
+                    tsteps_struct(nbins-(BstepX-1)).tsteps(tsteps_struct(nbins-BstepX+1).tcount) = Xstep;
+                    counts(nbins-(BstepX-1),nbins-(BstepY-1)) = counts(nbins-(BstepX-1))+1;
+
+                end;
+            end;
+            
+        end;
+   end;
+end;
