7a2e78a19d17ed63fd87d9514d04a55478e0185a
astrojim <james.m.mccracken@gmail.com>
2015-08-03 13:53:11 -0400

    Moved the noisy IR signal creation to a function

--- ThesisScripts/NoisyResponseExample.m
+++ ThesisScripts/NoisyResponseExample.m
@@ -15,22 +15,7 @@ hitspace = 5;
 hitpeak = 2;
 
 % build impulse signal
-hititer = 0;
-x = randn(1,liblength).*C;
-x(2) = hitpeak;
-for xiter = 2:1:liblength,
-    if( hititer > hitspace ),
-        x(xiter) = hitpeak;
-        hititer = 0;
-    end;
-    hititer = hititer + 1;
-end;
-
-% build noisy response
-y = zeros(1,length(x));
-for iter = 2:1:length(y),
-    y(iter) = x(iter-1) + B*randn();
-end;
+[x,y] = NoisyImpulseResponse(liblength,hitspace,hitpeak,C,B);
 
 %% transfer entropy (using JIDT)
 
